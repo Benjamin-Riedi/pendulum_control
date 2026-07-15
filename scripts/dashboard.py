95d596fbb022640a495e7a9c6b5f691dc8a8ae8e
Benjamin Riedi <briedi@ethz.ch>
2026-07-15 18:48:05 +0200

    dashboard node for process control via services

---
+++ scripts/dashboard.py
@@ -0,0 +1,67 @@
+import rospy
+from std_srvs.srv import Trigger
+
+class Dashboard:
+    def __init__(self):
+        self.init_proxies()
+
+    def init_proxies(self):
+        rospy.wait_for_service("/sensor/start", timeout=5)
+        # rospy.wait_for_service("/controller/start", timeout=5)
+        # rospy.wait_for_service("/homing/start", timeout=5)
+
+        self.start_sensor = rospy.ServiceProxy(
+            "/sensor/start",
+            Trigger
+        )
+        # self.start_homing = rospy.ServiceProxy(
+        #     "/homing/start",
+        #     Trigger
+        # )
+        # self.start_controller = rospy.ServiceProxy(
+        #     "/controller/start",
+        #     Trigger
+        # )
+    
+    def print_menu(self):
+        print("|Dashboard Menu:")
+        print("|----------------")
+        print("|s: Start Sensor. This first calibrates the frame, the pendulum must stand on the sensor in 2 seconds.")
+        print("|c: Start Controller. This starts sending motor commands based on the published angles")
+        print("|h: Start Homing.")
+        print("|q: Quit")
+    
+    def run(self):
+        while not rospy.is_shutdown():
+            self.print_menu()
+            cmd = input("> ")
+
+            if cmd == "s":
+                self.start_sensor()
+
+            elif cmd == "c":
+                self.start_controller()
+
+            elif cmd == "q":
+                break
+    def test(self):
+        while not rospy.is_shutdown():
+            self.print_menu()
+            cmd = input("> ")
+
+            if cmd == "s":
+                print("Starting sensor...")
+
+            elif cmd == "c":
+                print("Starting controller...")
+
+            elif cmd == "h":
+                print("Starting homing...")
+
+            elif cmd == "q":
+                break
+
+if __name__ == "__main__":
+    rospy.init_node("dashboard")
+    dashboard = Dashboard()
+    dashboard.run()
--- scripts/publisher.py
+++ scripts/publisher.py
@@ -3,12 +3,18 @@ import rospy
 from ethercat_motor_msgs.msg import MotorCtrlMessage
 from pendulum_control.msg import ArrayStamped
 from pendulum_control import set_pos, set_vel, set_torque, set_state
+from std_srvs.srv import Trigger, TriggerResponse
 
 class Publisher:
     def __init__(self):
         rospy.init_node("command_pub", anonymous=True)
         self.init_topics()
         self.init_publishers()
+        self.calib_srv = rospy.Service(
+            '/gelsight_rgb/start_calibration',
+            Trigger,
+            self.calibrate_cb
+        )
         rospy.sleep(5)  # wait for publishers to initialize
 
     def get_params(self):
@@ -23,9 +29,14 @@ class Publisher:
         self.motor_top_pub = rospy.Publisher(self.motor_topic, MotorCtrlMessage, queue_size=1)
         self.set_state_pub = rospy.Publisher(self.set_state_topic, ArrayStamped, queue_size=1)
 
+    def service_callback(self, req):
+        self.publish()
+        return TriggerResponse(success=True, message="Start command received.")
+
     def publish(self):
 
         ### SET STATE COMMANDS ###
+
 
         state_msg = set_state(x=0.0, phi=0.08, dx=0.0, dphi=0.0)
         self.set_state_pub.publish(state_msg)
@@ -56,6 +67,5 @@ class Publisher:
 
 if __name__ == "__main__":
     publisher = Publisher()
-    print('im alive')
-    publisher.publish()
+    rospy.spin()
 
